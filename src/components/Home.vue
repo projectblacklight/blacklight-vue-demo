57a809dd1e0c5561af0221631976c033936aef31
Justin Coyne <jcoyne@justincoyne.com>
2019-01-02 13:30:36 -0600

    Update per instructions

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <Search />
+  </div>
+</template>
+
+<script>
+
+import Search from 'blacklight-vue/src/components/Search'
+
+export default {
+  name: 'Home',
+  components: {
+    Search
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Blacklight from 'blacklight-vue'
+import BlacklightRoutes from 'blacklight-vue/src/routes'
+import VueRouter from 'vue-router'
+import Home from './components/Home.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(Blacklight)
+Vue.use(VueRouter)
+
+// Push your own "Home" page into the routes.
+BlacklightRoutes.push({ path: '/', name: 'home', component: Home })
+const router = new VueRouter({
+  routes: BlacklightRoutes
+})
+
+Vue.http.options.root = 'http://demo.projectblacklight.org'
+Vue.http.options.headers = {
+  Accept: 'application/json'
+}
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
